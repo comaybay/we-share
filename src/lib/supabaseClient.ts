756fee46240b51eac52fcbeb7ef93df97b1e0d5d
Thái Chí Bảo <comaybayy@gmail.com>
2022-09-22 17:35:42 +0700

    supabase setup

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabaseClient = createClient(
+	import.meta.env.VITE_SUPABASE_URL,
+	import.meta.env.VITE_SUPABASE_ANON_KEY,
+	{
+		persistSession: false,
+		autoRefreshToken: false
+	}
+);
