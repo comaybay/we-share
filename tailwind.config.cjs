e3cf655b4ef5b06fe3bdf7e05f08ac50ef448da0
Thái Chí Bảo <comaybayy@gmail.com>
2022-09-22 10:19:47 +0700

    overwrite tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,19 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'paper': '#FEFFFF',
+      'pri-base': '#32667C',
+      'pri-light': '#6493A7',
+      'pri-lighter': '#EEF5F8',
+      'sec-base': '#AB2B2B',
+      'tert-base': '#1512A',
+      'tert-light': '#D7D6FC',
+      'qua-base': '#1AA75B',
+      'quin-base': '#C6CA00',
+    },
   },
   plugins: [],
 }
