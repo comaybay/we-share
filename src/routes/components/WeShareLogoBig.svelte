ecc621eb402bb9f2d5c03b5806c2a8811e4405b6
Thái Chí Bảo <comaybayy@gmail.com>
2022-09-24 12:25:35 +0700

    WeShareIcon and logo

---
+++ src/routes/components/WeShareLogoBig.svelte
@@ -0,0 +1,8 @@
+<script>
+	import WeShareIcon from './WeShareIcon.svelte';
+</script>
+
+<a href="/" class="flex items-center h-fit">
+	<WeShareIcon size={64} strokeWidth={1} />
+	<span class="text-5xl tracking-[0.07em] text-pri-base">WeShare</span>
+</a>
