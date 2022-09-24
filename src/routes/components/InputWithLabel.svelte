6b40c2debaded669676987400c1d2875c9350e32
Thái Chí Bảo <comaybayy@gmail.com>
2022-09-24 12:25:48 +0700

    InputWithLabel

---
+++ src/routes/components/InputWithLabel.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let labelText: string;
+	export let inputName: string;
+</script>
+
+<label>
+	<p class="text-lg">{labelText}</p>
+	<input name={inputName} type="text" class="rounded-sm w-full" />
+</label>
