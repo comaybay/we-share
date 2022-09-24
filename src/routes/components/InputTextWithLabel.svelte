7c896a14ab6d5490dd8e8985ac47df945904c470
Thái Chí Bảo <comaybayy@gmail.com>
2022-09-24 14:55:48 +0700

    rename InputWithLabel -> InputWithTextLabel and allow 'string' only types

---
+++ src/routes/components/InputTextWithLabel.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let type: 'text' | 'password' | 'email' = 'text';
+	export let labelText: string;
+	export let name: string;
+
+	export let required = false;
+	export let value: string;
+</script>
+
+<label>
+	<p class="text-lg">
+		{#if required}
+			<span class="text-sec-base">*</span>
+		{/if}
+		{labelText}
+	</p>
+	{#if type === 'text'}
+		<input {name} bind:value type="text" class="rounded-sm w-full" />
+	{:else if type === 'password'}
+		<input {name} bind:value type="password" class="rounded-sm w-full" />
+	{:else}
+		<input {name} bind:value type="email" class="rounded-sm w-full" />
+	{/if}
+</label>
--- src/routes/components/InputWithLabel.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let labelText: string;
-	export let inputName: string;
-</script>
-
-<label>
-	<p class="text-lg">{labelText}</p>
-	<input name={inputName} type="text" class="rounded-sm w-full" />
-</label>
