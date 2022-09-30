324ba0eb6332a68bbb637d60aa09144700d34b8c
Thái Chí Bảo <comaybayy@gmail.com>
2022-09-30 23:23:28 +0700

    frontend: update header UI

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import Header from '../components/header/Header.svelte';
+	import type { LayoutServerData } from './$types';
+
+	export let data: LayoutServerData;
 </script>
 
-<Header />
+<Header userProfile={data.userProfile} />
 <slot />
