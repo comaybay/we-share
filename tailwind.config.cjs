e82284b953f77eb1b21fcb9f15501aeea7aaaa80
Thái Chí Bảo <comaybayy@gmail.com>
2022-10-01 20:49:33 +0700

    update sans-serif font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['sans-serif', ...defaultTheme.fontFamily.sans],
+      }
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
