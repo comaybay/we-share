f2c2b1fec626465f88d52956f585dce9d3d061d5
comaybay <comaybayy@gmail.com>
2022-10-10 23:29:36 +0700

    fix page not cover entire page on mobile

--- src/routes/dang-ky/+page.svelte
+++ src/routes/dang-ky/+page.svelte
@@ -43,110 +43,112 @@
 	</div>
 	<div class="absolute -z-10 h-full ml-20 border-r-2 border-sec-base" />
 	<div
-		class="p-4 min-h-screen min-w-fit flex flex-col lg:flex-row lg:space-x-20 justify-center items-center"
+		class="p-4 min-h-screen flex flex-col justify-center lg:flex-row lg:space-x-20 lg:items-center"
 	>
 		<div class="m-4 lg:-translate-y-52">
 			<WeShareLogoBig />
 		</div>
-		<form
-			use:enhance={handleSubmit}
-			method="POST"
-			class="shrink w-[29rem] min-w-[22rem] h-fit px-8 pb-6 border border-pri-light rounded-sm bg-paper"
-		>
-			<h3 class="text-center text-3xl my-6">Đăng ký</h3>
-			<div class="flex flex-col space-y-4">
-				<div>
+		<div class="flex justify-center">
+			<form
+				use:enhance={handleSubmit}
+				method="POST"
+				class="shrink w-[29rem] min-w-[22rem] h-fit px-8 pb-6 border border-pri-light rounded-sm bg-paper"
+			>
+				<h3 class="text-center text-3xl my-6">Đăng ký</h3>
+				<div class="flex flex-col space-y-4">
+					<div>
+						<InputTextWithLabel
+							labelText="Tên tài khoản"
+							name="username"
+							bind:value={username}
+							required
+							minlength={4}
+							maxlength={15}
+						/>
+						{#if form?.usernameMissing}
+							<p class="text-sec-base">*Mục này là bắt buộc</p>
+						{:else if form?.userNameAlreadyExists}
+							<p class="text-sec-base">*Tên tài khoản này đã tồn tại</p>
+						{:else if form?.usernameInvalid}
+							<p class="text-sec-base">
+								*Tên tài khoản không hợp lệ (chỉ nhận ký tự không dấu hoặc số, không nhận dấu cách
+								hay ký tự đặc biệt trừ dấu "_", trong khoảng 6-15 ký tự)
+							</p>
+						{/if}
+					</div>
+
+					<div>
+						<InputTextWithLabel
+							labelText="Mật khẩu"
+							name="password"
+							type="password"
+							bind:value={password}
+							required
+							minlength={6}
+							maxlength={50}
+						/>
+						{#if form?.passwordMissing}
+							<p class="text-sec-base">*Mục này là bắt buộc</p>
+						{/if}
+						{#if form?.passwordInvalid}
+							<p class="text-sec-base">
+								*Mật khẩu không hợp lệ (không chứa ký tự đặc biệt, mật khẩu trong khoảng 6-50 ký tự)
+							</p>
+						{/if}
+					</div>
+
+					<div>
+						<InputTextWithLabel
+							labelText="Nhập lại mật khẩu"
+							name="reenter-password"
+							type="password"
+							bind:value={reenterPassword}
+							required
+							minlength={6}
+							maxlength={50}
+						/>
+						{#if password != reenterPassword && pressedOnce}
+							<p class="text-sec-base">*Mật khẩu nhập lại không đúng</p>
+						{/if}
+					</div>
+
+					<div>
+						<InputTextWithLabel
+							labelText="Email"
+							name="email"
+							type="email"
+							bind:value={email}
+							required
+							minlength={5}
+							maxlength={254}
+						/>
+						{#if form?.emailMissing}
+							<p class="text-sec-base">*Mục này là bắt buộc</p>
+						{:else if form?.emailInvalid}
+							<p class="text-sec-base">*Email không hợp lệ</p>
+						{/if}
+					</div>
+
 					<InputTextWithLabel
-						labelText="Tên tài khoản"
-						name="username"
-						bind:value={username}
-						required
-						minlength={4}
-						maxlength={15}
+						labelText="Tên hiển thị của bạn"
+						name="profilename"
+						bind:value={profilename}
 					/>
-					{#if form?.usernameMissing}
-						<p class="text-sec-base">*Mục này là bắt buộc</p>
-					{:else if form?.userNameAlreadyExists}
-						<p class="text-sec-base">*Tên tài khoản này đã tồn tại</p>
-					{:else if form?.usernameInvalid}
-						<p class="text-sec-base">
-							*Tên tài khoản không hợp lệ (chỉ nhận ký tự không dấu hoặc số, không nhận dấu cách hay
-							ký tự đặc biệt trừ dấu "_", trong khoảng 6-15 ký tự)
-						</p>
-					{/if}
-				</div>
 
-				<div>
-					<InputTextWithLabel
-						labelText="Mật khẩu"
-						name="password"
-						type="password"
-						bind:value={password}
-						required
-						minlength={6}
-						maxlength={50}
-					/>
-					{#if form?.passwordMissing}
-						<p class="text-sec-base">*Mục này là bắt buộc</p>
-					{/if}
-					{#if form?.passwordInvalid}
-						<p class="text-sec-base">
-							*Mật khẩu không hợp lệ (không chứa ký tự đặc biệt, mật khẩu trong khoảng 6-50 ký tự)
-						</p>
-					{/if}
+					<InputTextWithLabel labelText="Danh ngôn của bạn" name="quote" bind:value={quote} />
 				</div>
-
-				<div>
-					<InputTextWithLabel
-						labelText="Nhập lại mật khẩu"
-						name="reenter-password"
-						type="password"
-						bind:value={reenterPassword}
-						required
-						minlength={6}
-						maxlength={50}
-					/>
-					{#if password != reenterPassword && pressedOnce}
-						<p class="text-sec-base">*Mật khẩu nhập lại không đúng</p>
+				<div class="mt-8 flex justify-end space-x-2">
+					{#if form?.userFriendlyMessage}
+						<p class="mr-auto font-semibold text-sec-base">*{form.userFriendlyMessage}</p>
 					{/if}
+					<div class="shrink-0">
+						<Button {loading}><span class="px-4">Đăng ký</span></Button>
+					</div>
 				</div>
-
-				<div>
-					<InputTextWithLabel
-						labelText="Email"
-						name="email"
-						type="email"
-						bind:value={email}
-						required
-						minlength={5}
-						maxlength={254}
-					/>
-					{#if form?.emailMissing}
-						<p class="text-sec-base">*Mục này là bắt buộc</p>
-					{:else if form?.emailInvalid}
-						<p class="text-sec-base">*Email không hợp lệ</p>
-					{/if}
+				<div class="mt-8 w-fit mx-auto">
+					<Link href="/dang-nhap">Đã có tài khoản? đăng nhập!</Link>
 				</div>
-
-				<InputTextWithLabel
-					labelText="Tên hiển thị của bạn"
-					name="profilename"
-					bind:value={profilename}
-				/>
-
-				<InputTextWithLabel labelText="Danh ngôn của bạn" name="quote" bind:value={quote} />
-			</div>
-			<div class="mt-8 flex justify-end space-x-2">
-				{#if form?.userFriendlyMessage}
-					<p class="mr-auto font-semibold text-sec-base">*{form.userFriendlyMessage}</p>
-				{/if}
-				<div class="shrink-0">
-					<Button {loading}><span class="px-4">Đăng ký</span></Button>
-				</div>
-			</div>
-			<div class="mt-8 w-fit mx-auto">
-				<Link href="/dang-nhap">Đã có tài khoản? đăng nhập!</Link>
-			</div>
-		</form>
+			</form>
+		</div>
 	</div>
 </div>
--- src/routes/dang-nhap/+page.svelte
+++ src/routes/dang-nhap/+page.svelte
@@ -34,62 +34,64 @@
 	</div>
 	<div class="absolute -z-10 h-full ml-20 border-r-2 border-sec-base" />
 
-	<div class="p-4 min-h-screen min-w-fit flex flex-col items-center justify-center">
+	<div class="p-4 min-h-screen flex flex-col justify-center">
 		<WeShareLogoBig />
-		<form
-			use:enhance={handleSubmit}
-			method="POST"
-			class="mt-10 w-[29rem] min-w-[22rem] px-8 pb-6 border border-pri-light rounded-sm bg-paper"
-		>
-			<h3 class="text-center text-3xl my-6">Đăng nhập</h3>
-			<div class="flex flex-col space-y-4">
-				<div>
-					<InputTextWithLabel
-						bind:value={usernameOrEmail}
-						labelText="Tên tài khoản hoặc email"
-						name="username-or-email"
-						type="text"
-						minlength={4}
-						maxlength={254}
-					/>
-					{#if form?.usernameOrEmailMissing}
-						<p class="text-sec-base">*Mục này là bắt buộc</p>
-					{:else if form?.usernameOrEmailInvalid}
-						<p class="text-sec-base">*Tên tài khoản hoặc email không hợp lệ</p>
-					{:else if form?.usernameNotExist}
-						<p class="text-sec-base">*Tên tài khoản không tồn tại</p>
+		<div class="flex justify-center">
+			<form
+				use:enhance={handleSubmit}
+				method="POST"
+				class="shrink mt-10 w-[29rem] min-w-[22rem] px-8 pb-6 border border-pri-light rounded-sm bg-paper"
+			>
+				<h3 class="text-center text-3xl my-6">Đăng nhập</h3>
+				<div class="flex flex-col space-y-4">
+					<div>
+						<InputTextWithLabel
+							bind:value={usernameOrEmail}
+							labelText="Tên tài khoản hoặc email"
+							name="username-or-email"
+							type="text"
+							minlength={4}
+							maxlength={254}
+						/>
+						{#if form?.usernameOrEmailMissing}
+							<p class="text-sec-base">*Mục này là bắt buộc</p>
+						{:else if form?.usernameOrEmailInvalid}
+							<p class="text-sec-base">*Tên tài khoản hoặc email không hợp lệ</p>
+						{:else if form?.usernameNotExist}
+							<p class="text-sec-base">*Tên tài khoản không tồn tại</p>
+						{/if}
+					</div>
+
+					<div>
+						<InputTextWithLabel
+							bind:value={password}
+							labelText="Mật khẩu"
+							name="password"
+							type="password"
+							minlength={6}
+							maxlength={50}
+						/>
+						{#if form?.passwordMissing}
+							<p class="text-sec-base">*Mục này là bắt buộc</p>
+						{:else if form?.passwordInvalid}
+							<p class="text-sec-base">*Mật khẩu không hợp lệ</p>
+						{/if}
+					</div>
+				</div>
+
+				<div class="mt-8 flex justify-end space-x-2">
+					{#if form?.userFriendlyMessage}
+						<p class="mr-auto font-semibold text-sec-base">*{form.userFriendlyMessage}</p>
 					{/if}
+					<div class="shrink-0 w-fit ml-auto">
+						<Button {loading}><span class="mx-4">Đăng nhập</span></Button>
+					</div>
 				</div>
 
-				<div>
-					<InputTextWithLabel
-						bind:value={password}
-						labelText="Mật khẩu"
-						name="password"
-						type="password"
-						minlength={6}
-						maxlength={50}
-					/>
-					{#if form?.passwordMissing}
-						<p class="text-sec-base">*Mục này là bắt buộc</p>
-					{:else if form?.passwordInvalid}
-						<p class="text-sec-base">*Mật khẩu không hợp lệ</p>
-					{/if}
+				<div class="mt-8 block w-fit mx-auto">
+					<Link href="/dang-ky">Chưa có tài khoản? đăng ký!</Link>
 				</div>
-			</div>
-
-			<div class="mt-8 flex justify-end space-x-2">
-				{#if form?.userFriendlyMessage}
-					<p class="mr-auto font-semibold text-sec-base">*{form.userFriendlyMessage}</p>
-				{/if}
-				<div class="shrink-0 w-fit ml-auto">
-					<Button {loading}><span class="mx-4">Đăng nhập</span></Button>
-				</div>
-			</div>
-
-			<div class="mt-8 block w-fit mx-auto">
-				<Link href="/dang-ky">Chưa có tài khoản? đăng ký!</Link>
-			</div>
-		</form>
+			</form>
+		</div>
 	</div>
 </div>
