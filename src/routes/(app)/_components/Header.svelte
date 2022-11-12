8c208a4292bea3430812eec9a70a254d65f38509
comaybay <comaybayy@gmail.com>
2022-11-12 22:33:41 +0700

    header: make user profile picture it's own component

--- src/routes/(app)/_components/Header.svelte
+++ src/routes/(app)/_components/Header.svelte
@@ -5,6 +5,7 @@
 	import MobileNavigation from './header/MobileNavigation.svelte';
 	import NavLink from './header/NavLink.svelte';
 	import UserMenuButton from './header/UserMenuButton.svelte';
+	import UserProfilePicture from './UserProfilePicture.svelte';
 </script>
 
 <div class="fixed w-full">
@@ -30,20 +31,7 @@
 					<a href="/dang-nhap">
 						<ButtonOutline>
 							<div class="">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1"
-									stroke="currentColor"
-									class="w-8 h-8 inline-block"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-									/>
-								</svg>
+								<UserProfilePicture />
 								<span class="align-middle text-lg uppercase">Đăng nhập</span>
 							</div>
 						</ButtonOutline>
