5a126fb3fdaef276722b729ea53023ca881546be
comaybay <comaybayy@gmail.com>
2022-11-12 22:23:11 +0700

    update useProtectedRoute

--- src/lib/useProtectedRoute.ts
+++ src/lib/useProtectedRoute.ts
@@ -1,9 +1,15 @@
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 import { redirect, type LoadEvent, type RequestEvent } from '@sveltejs/kit';
 
+/**
+ * redirects to login page if not logged in
+ * @returns session and supabase client, guaranteed to not be null
+ */
 export async function useProtectedRoute(event: LoadEvent | RequestEvent) {
-	const { session } = await getSupabase(event);
+	const { session, supabaseClient } = await getSupabase(event);
 	if (!session) {
 		throw redirect(303, `/dang-nhap?returnto=${encodeURIComponent(event.url.pathname)}`);
 	}
+
+	return { session, supabaseClient };
 }
