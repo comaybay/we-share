06defb91f9a63af2716ded181dbff1906f21f0f7
comaybay <comaybayy@gmail.com>
2022-11-12 22:23:25 +0700

    get question from slug and username

---
+++ src/routes/(app)/hoi-dap/[username]/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	console.log(data);
+</script>
+
+{#if data}
+	<p>{data.post.title}</p>
+	{@html data.post.content}
+{/if}
