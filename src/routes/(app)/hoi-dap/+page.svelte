75fcfa04a12dfc642777e8006d6b720fd1d6a372
comaybay <comaybayy@gmail.com>
2022-11-19 22:37:42 +0700

    basic questions page

---
+++ src/routes/(app)/hoi-dap/+page.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+	import { goto, invalidate } from '$app/navigation';
+	import { navigating, page } from '$app/stores';
+	import Button from 'src/routes/_components/buttons/Button.svelte';
+	import ButtonText from 'src/routes/_components/buttons/ButtonText.svelte';
+	import SparkleIcon from '../_components/icons/SparkleIcon.svelte';
+	import TopIcon from '../_components/icons/TopIcon.svelte';
+	import TopicContainer from '../_components/posts/TopicContainer.svelte';
+	import type { PageData } from './$types';
+	import Question from './_components/Question.svelte';
+
+	export let data: PageData;
+
+	let loading = true;
+
+	let questions: PageData['questions'];
+
+	$: loading = $navigating !== null;
+	$: questions = data.questions;
+	$: url = $page.url;
+	$: console.log($navigating);
+
+	let topics: string[] = [];
+	$: {
+		if (questions) {
+			topics = [];
+			//get 1 topic of each question, not including duplicates
+			const questionWithTopics = questions.filter(q => q.topics.length > 0);
+			for (const question of questionWithTopics) {
+				const topic = question.topics.find(t => !topics.includes(t));
+				if (topic) {
+					topics.push(topic);
+				}
+			}
+		}
+	}
+
+	let newestQuestionsActive = false;
+	let topQuestionsActive = false;
+	$: {
+		const sortSrder = url.searchParams.get('order');
+		newestQuestionsActive = sortSrder !== null && sortSrder === 'newest';
+		topQuestionsActive = sortSrder !== null && sortSrder === 'top';
+	}
+
+	async function onClickNewestQuestions() {
+		url.searchParams.set('order', 'newest');
+		url = url;
+		updateData();
+	}
+
+	async function onClickTopQuestions() {
+		url.searchParams.set('order', 'top');
+		url = url;
+		updateData();
+	}
+
+	async function updateData() {
+		await goto(url.href, { keepfocus: true, noscroll: true });
+		await invalidate('/page');
+	}
+</script>
+
+<div class="flex flex-col lg:flex-row-reverse gap-x-16 px-4 md:px-12 justify-between">
+	<div class="flex flex-col shrink-0 min-w-[330px] mb-4 lg:mb-0">
+		<Button>
+			<div class="w-full flex gap-x-2 justify-center items-center px-14">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 00-1.032-.211 50.89 50.89 0 00-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 002.433 3.984L7.28 21.53A.75.75 0 016 21v-4.03a48.527 48.527 0 01-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979z"
+					/>
+					<path
+						d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 001.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0015.75 7.5z"
+					/>
+				</svg>
+
+				<b>ĐẶT CÂU HỎI</b>
+			</div>
+		</Button>
+		<div class="mt-4 border border-pri-base">
+			<p class="py-2 px-4 text-xl font-sans font-thin bg-pri-base text-paper">Chủ đề</p>
+			<div class="px-4 py-3">
+				<TopicContainer baseHref="/hoi-dap" {topics} />
+			</div>
+		</div>
+	</div>
+	<div>
+		<ButtonText active={newestQuestionsActive} on:click={onClickNewestQuestions}>
+			<div class="flex gap-x-1">
+				<SparkleIcon />
+				<span>Mới nhất</span>
+			</div>
+		</ButtonText>
+		<ButtonText active={topQuestionsActive} on:click={onClickTopQuestions}>
+			<div class="flex gap-x-1">
+				<TopIcon />
+				<span>Top</span>
+			</div>
+		</ButtonText>
+		{#if !loading && data}
+			{#each questions as question}
+				<Question {question} />
+			{/each}
+		{:else}
+			<div class="mt-8 flex items-center gap-x-2">
+				<svg
+					aria-hidden="true"
+					class="inline-block mr-2 w-8 h-8 animate-spin"
+					viewBox="0 0 24 24"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<circle class="opacity-25 stroke-pri-base" cx="12" cy="12" r="10" stroke-width="2" />
+					<path
+						class="opacity-75 fill-sec-base"
+						d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+					/>
+				</svg>
+				<span class="text-lg"> Đang tải... </span>
+			</div>
+		{/if}
+	</div>
+</div>
