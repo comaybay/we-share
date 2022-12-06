a91daf079f10ef6297dba68d0646293b63e43271
comaybay <comaybayy@gmail.com>
2022-12-06 21:08:55 +0700

    refactoring: turn sections into it's own components

---
+++ src/routes/(app)/_components/posts/TopPostsButton.svelte
@@ -0,0 +1,13 @@
+<script>
+	import ButtonText from 'src/routes/_components/buttons/ButtonText.svelte';
+	import TopIcon from '../icons/TopIcon.svelte';
+
+	export let active = false;
+</script>
+
+<ButtonText {active} on:click>
+	<div class="flex gap-x-1">
+		<TopIcon />
+		<span>Top</span>
+	</div>
+</ButtonText>
