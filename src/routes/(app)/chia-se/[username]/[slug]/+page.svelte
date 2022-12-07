8d473343d451e6a351833b7114288d9f7fe88f69
comaybay <comaybayy@gmail.com>
2022-12-07 12:37:17 +0700

    sharing post detail page logic

---
+++ src/routes/(app)/chia-se/[username]/[slug]/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { toRelativeTime } from 'src/lib/i18n/toRelativeTime';
+	import PostTitle from 'src/routes/(app)/_components/postDetail/PostTitle.svelte';
+	import TopicContainer from 'src/routes/(app)/_components/postDetail/TopicContainer.svelte';
+	import UserProfilePicture from 'src/routes/_components/UserProfilePicture.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	let post: PageData['post'] | null = null;
+	$: {
+		({ post } = data);
+	}
+</script>
+
+<div class="flex justify-center">
+	{#if post}
+		<div class="grow px-8 md:py-4 md:border md:border-pri-light max-w-3xl min-w-0">
+			<a href="/nguoi-dung/{post.author.username}">
+				<div class="inline-block w-12 h-12 mb-1">
+					<UserProfilePicture />
+				</div>
+				<span class="font-bold">
+					{post.author.username}
+				</span>
+			</a>
+			<span>{toRelativeTime(post.dateCreated)}</span>
+			<PostTitle>{post.title}</PostTitle>
+			<div class="mt-4 mb-6">
+				{@html post.content}
+			</div>
+
+			<TopicContainer topics={post.topics} baseHref="/chia-se" />
+		</div>
+	{/if}
+</div>
