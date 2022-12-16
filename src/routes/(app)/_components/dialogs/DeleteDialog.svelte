60b0df31b4e1e8635f9ffdc23bbf6d3cd4d53f59
comaybay <comaybayy@gmail.com>
2022-12-16 21:05:11 +0700

    dialogs

---
+++ src/routes/(app)/_components/dialogs/DeleteDialog.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Button from 'src/routes/_components/buttons/Button.svelte';
+	import ButtonText from 'src/routes/_components/buttons/ButtonText.svelte';
+	import WarningIcon from '../icons/WarningIcon.svelte';
+
+	export let message: string;
+	export let onConfirm: (e: MouseEvent) => void;
+	export let onCancel: (e: MouseEvent) => void;
+</script>
+
+<div>
+	<WarningIcon className="mx-auto h-16 text-sec-base" />
+	<span>{message}</span>
+
+	<div class="border-t border-pri-light pt-2 mt-3 flex justify-end gap-2">
+		<ButtonText on:click={onCancel}><div class="h-full font-bold">Hủy</div></ButtonText>
+		<Button on:click={onConfirm}><span class="font-bold">Xóa</span></Button>
+	</div>
+</div>
