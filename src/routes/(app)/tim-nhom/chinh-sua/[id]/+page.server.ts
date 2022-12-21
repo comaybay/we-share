0e91789e24fb0e640e6f332c53cba4e91c3e1537
comaybay <comaybayy@gmail.com>
2022-12-21 20:18:52 +0700

    edit find team post page/logic

---
+++ src/routes/(app)/tim-nhom/chinh-sua/[id]/+page.server.ts
@@ -0,0 +1,142 @@
+import { invalid, redirect, type Actions } from '@sveltejs/kit';
+import { MAX_NUMBER_OF_TOPICS } from 'src/lib/constants';
+import { error500 } from 'src/lib/errors';
+import {
+	MSG_API_PERMISSION_DENIED,
+	MSG_SERVER_ERROR,
+	MSG_SERVER_ERROR_TRY_AGAIN
+} from 'src/lib/messages';
+import slugify from 'src/lib/server/slugify';
+import { validateCommonPostProps } from 'src/lib/server/validations';
+import type { FindTeamPostFormError } from 'src/lib/types/FindTeamPostFormError';
+import type { ForeignTableCount } from 'src/lib/types/supabase/ForeignTableCount';
+import { useProtectedRoute } from 'src/lib/useProtectedRoute';
+
+export const actions: Actions = {
+	async default(event) {
+		const { session, supabaseClient } = await useProtectedRoute(event);
+
+		const formData = await event.request.formData();
+		const title = formData.get('title')?.toString()?.trim() as string;
+		const content = formData.get('content')?.toString()?.trim() as string;
+		const textContent = formData.get('text-content')?.toString()?.trim() as string;
+		const courseCode = formData.get('course-code')?.toString()?.trim() as string;
+		const teamSize = parseInt(formData.get('team-size')?.toString()?.trim() as string);
+		const postId = formData.get('post-id')?.toString()?.trim() as string;
+
+		if (isNaN(teamSize)) {
+			return invalid(400, { message: 'invalid team size' });
+		}
+
+		let neededSkills: string[];
+		try {
+			neededSkills = JSON.parse(formData.get('needed-skills') as string);
+
+			if (neededSkills.length > MAX_NUMBER_OF_TOPICS) {
+				return invalid(400, {
+					message: `number of skills must not be larger than ${MAX_NUMBER_OF_TOPICS}`
+				});
+			}
+		} catch (e) {
+			return invalid(400, { message: 'invalid skills' });
+		}
+
+		const result: EditFindTeamPostFormError = validateCommonPostProps({
+			content,
+			textContent,
+			title
+		});
+
+		if (!courseCode) {
+			result.courseCodeEmpty = true;
+		}
+
+		if (result.contentEmpty || result.titleEmpty || result.courseCodeEmpty) {
+			return invalid(400, result);
+		}
+
+		const { data, error } = await supabaseClient
+			.from('post_teams')
+			.select('slug, author_id, member_count:profiles!post_team_members(count)')
+			.match({ id: postId });
+
+		if (error) {
+			throw error500({ message: MSG_SERVER_ERROR });
+		}
+
+		if (data.length === 0) {
+			return invalid(400, { message: 'id not found' });
+		}
+
+		if (data[0].author_id !== session.user.id) {
+			throw error500({ message: MSG_API_PERMISSION_DENIED });
+		}
+
+		if ((data[0].member_count as ForeignTableCount)[0].count > teamSize) {
+			result.teamSizeSmallerThanMemberCount = true;
+			return invalid(400, result);
+		}
+
+		const oldSlug = data[0].slug;
+		let newSlug = slugify(title);
+
+		if (newSlug.length === 0) {
+			newSlug = Date.now().toString();
+		} else if (oldSlug !== newSlug) {
+			const { error: countError, data: duplicatedTitleCount } = await supabaseClient
+				.rpc('post_teams_count_duplicated_slug', {
+					_author_id: session.user.id,
+					_slug: newSlug
+				})
+				.single();
+
+			if (countError) {
+				result.serverError = true;
+				result.userFriendlyMessage = MSG_SERVER_ERROR_TRY_AGAIN;
+				return invalid(500, result);
+			}
+
+			if (duplicatedTitleCount > 0) {
+				newSlug = `${newSlug}--${duplicatedTitleCount + 1}`;
+			}
+		}
+
+		const { error: insertError } = await supabaseClient
+			.from('post_teams')
+			.update({
+				slug: newSlug,
+				text_content: textContent,
+				date_last_updated: new Date().toISOString(),
+				needed_skills: neededSkills,
+				course_code: courseCode,
+				team_size: teamSize,
+				title,
+				content
+			})
+			.match({ id: postId });
+
+		if (insertError) {
+			result.serverError = true;
+			result.userFriendlyMessage = MSG_SERVER_ERROR_TRY_AGAIN;
+			return invalid(500, result);
+		}
+
+		const { error: getUsernameError, data: userProfile } = await supabaseClient
+			.from('profiles')
+			.select('username')
+			.match({ id: session.user.id })
+			.single();
+
+		if (getUsernameError || !userProfile) {
+			result.serverError = true;
+			result.userFriendlyMessage = MSG_SERVER_ERROR_TRY_AGAIN;
+			return invalid(500, result);
+		}
+
+		throw redirect(303, `/tim-nhom/${userProfile.username}/${newSlug}`);
+	}
+};
+
+type EditFindTeamPostFormError = FindTeamPostFormError & {
+	teamSizeSmallerThanMemberCount?: true;
+};
---
+++ src/routes/(app)/tim-nhom/chinh-sua/[id]/+page.svelte
@@ -0,0 +1,261 @@
+<script lang="ts">
+	import { applyAction, enhance } from '$app/forms';
+	import {
+		MAX_NUMBER_OF_TOPICS,
+		MAX_TEAM_SIZE,
+		MIN_TEAM_SIZE,
+		POST_QUESTION_MAX_LENGTH,
+		POST_TEAM_MAX_LENGTH,
+		TOPIC_MAX_LENGTH
+	} from 'src/lib/constants';
+	import { userFriendlyMessage } from 'src/lib/userFriendlyMessage';
+	import PostEditor from 'src/routes/(app)/_components/posts/forms/PostEditor.svelte';
+	import { flip } from 'svelte/animate';
+	import Button from '../../../../_components/buttons/Button.svelte';
+	import InputTitle from '../../../_components/posts/forms/InputTitle.svelte';
+	import NewPostHeader from '../../../_components/posts/forms/NewPostHeader.svelte';
+	import type { ActionData, PageData } from './$types';
+
+	export let form: ActionData;
+	export let data: PageData;
+
+	const post = data.post;
+	$: postError = form;
+
+	let courseCode = post.courseCode;
+	let title = post.title;
+	let content = post.content;
+	let textContent = '';
+	let teamSize: number | null = post.teamSize;
+
+	let inputTopic = '';
+	let neededSkills: string[] = post.neededSkills;
+
+	let submitting = false;
+
+	$: skillLimitReached = neededSkills.length >= MAX_NUMBER_OF_TOPICS;
+	$: contentTooLong = textContent.length > POST_QUESTION_MAX_LENGTH;
+
+	function addNeededSkills() {
+		const newSkill = inputTopic.trim();
+		if (!newSkill) {
+			return;
+		}
+
+		if (!neededSkills.includes(newSkill)) {
+			neededSkills.push(newSkill);
+			neededSkills = neededSkills;
+		}
+
+		inputTopic = '';
+	}
+
+	function removeNeededSkill(index: number) {
+		neededSkills.splice(index, 1);
+		neededSkills = neededSkills;
+	}
+</script>
+
+<NewPostHeader>Chỉnh sửa bài viết tìm nhóm</NewPostHeader>
+
+<div class="mx-auto px-8 max-w-6xl">
+	<form
+		method="post"
+		use:enhance={({ data }) => {
+			submitting = true;
+			//add aditional values to the form data
+			data.append('post-id', post.id.toString());
+			data.append('needed-skills', JSON.stringify(neededSkills));
+			data.append('content', content);
+			data.append('text-content', textContent);
+
+			return async ({ result }) => {
+				await applyAction(result);
+				submitting = false;
+			};
+		}}
+	>
+		<div class="flex gap-2">
+			<div>
+				<div class="flex items-center gap-1">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z"
+						/>
+					</svg>
+					<p><span class="text-sec-base">*</span>Mã lớp:</p>
+				</div>
+				<input
+					disabled={submitting}
+					required
+					name="course-code"
+					minlength={3}
+					maxlength={255}
+					bind:value={courseCode}
+					type="text"
+					class="mt-1 rounded-sm"
+					placeholder="Mã lớp"
+				/>
+			</div>
+			<div>
+				<div class="flex items-center gap-1">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+						/>
+					</svg>
+					<p><span class="text-sec-base">*</span>Số lượng thành viên:</p>
+				</div>
+				<input
+					disabled={submitting}
+					required
+					name="team-size"
+					min={MIN_TEAM_SIZE}
+					max={MAX_TEAM_SIZE}
+					type="number"
+					bind:value={teamSize}
+					on:change={() => (teamSize = teamSize ? parseInt(teamSize.toString()) : null)}
+					class="mt-1 rounded-sm w-full"
+					placeholder="từ 2-100 thành viên"
+				/>
+			</div>
+		</div>
+
+		{#if postError?.teamSizeSmallerThanMemberCount}
+			<p class="text-sec-base">
+				*Số lượng thành viên mới không được nhỏ hơn số lượng thành viên hiện tại
+			</p>
+		{/if}
+
+		{#if postError?.courseCodeEmpty}
+			<p class="text-sec-base">*Mã lớp không được để trống</p>
+		{/if}
+
+		<div class="mt-4">
+			<InputTitle {title} disabled={submitting} />
+			{#if postError?.titleEmpty}
+				<p class="text-sec-base">
+					*{userFriendlyMessage.titleEmpty}
+				</p>
+			{/if}
+		</div>
+
+		<div class="mt-4">
+			<PostEditor
+				placeholder="Nội dung"
+				disabled={submitting}
+				bind:text={textContent}
+				bind:value={content}
+			/>
+		</div>
+
+		<div class="mt-2">
+			<span class:text-sec-base={contentTooLong}>
+				{textContent.length} / {POST_TEAM_MAX_LENGTH}
+			</span>
+			{#if contentTooLong}
+				<span class={contentTooLong ? 'ml-2 text-sec-base font-bold' : ''}>
+					Số lượng ký tự đã vượt quá giới hạn cho phép
+				</span>
+			{/if}
+			{#if postError?.contentEmpty}
+				<p class="text-sec-base">
+					*{userFriendlyMessage.contentEmpty}
+				</p>
+			{/if}
+		</div>
+
+		<div class="mt-4 flex flex-wrap gap-2">
+			{#if !skillLimitReached}
+				<div
+					class="topic-box flex items-center w-fit rounded-full border border-pri-light 
+    hover:enabled:border-pri-base focus-within:border-tert-base hover:enabled:focus-within:border-tert-base"
+				>
+					<input
+						minlength={3}
+						maxlength={TOPIC_MAX_LENGTH}
+						type="text"
+						class="pl-4 pr-2 rounded-full w-36 border-none focus:outline-none resize-x"
+						placeholder="Kỹ năng cần tìm"
+						disabled={submitting}
+						bind:value={inputTopic}
+					/>
+
+					<button
+						disabled={submitting}
+						type="button"
+						on:click={addNeededSkills}
+						class="shrink-0 w-10 h-10 rounded-full text-pri-base hover:enabled:bg-pri-lighter disabled:text-pri-light"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							class="h-full w-full p-3"
+						>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+						</svg>
+					</button>
+				</div>
+
+				{#if neededSkills.length > 0}
+					<div class="mx-4 border-l border-tert-base" />
+				{/if}
+			{/if}
+
+			{#each neededSkills as topic, i (topic)}
+				<div
+					class="flex items-center rounded-full border border-pri-light bg-paper min-w-0"
+					animate:flip={{ duration: 600 }}
+				>
+					<span class="pl-4 pr-2 truncate">{topic}</span>
+					<button
+						on:click={() => removeNeededSkill(i)}
+						class="w-10 h-10 rounded-full hover:bg-pri-lighter"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							class="h-full w-full p-3"
+						>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</button>
+				</div>
+			{/each}
+		</div>
+
+		<div class="flex justify-between mt-4">
+			<p class="text-sec-base font-semibold">
+				{#if postError?.userFriendlyMessage}
+					*{postError.userFriendlyMessage}
+				{/if}
+			</p>
+			<Button loading={submitting} disabled={contentTooLong}><span class="px-12">Đăng</span></Button
+			>
+		</div>
+	</form>
+</div>
